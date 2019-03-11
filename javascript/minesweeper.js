9efb19567e65871f7b8407c01d525b1deb154ecd
Andrew Asplund <andrew.j.asplund@gmail.com>
2019-03-10 17:53:41 -0700

    Completed win and lose cond
    The game now recognizes a win and a lose. Some of the JavaScript is still a mess because I was in the process of moving things around.

--- javascript/minesweeper.js
+++ javascript/minesweeper.js
@@ -1,12 +1,21 @@
+// It seems silly putting it up here, but...
+let currentGame;
+
 class Minesweeper
 {
     constructor(xSize, ySize, initBombs)
     {
         this.xSize = xSize;
         this.ySize = ySize;
+        console.log("board size: " + this.xSize + "x" + this.ySize);
         this.bombCount = initBombs;
+        this.firstMove = true;
+    
+        this.revealedTiles = 0;
 
         this.gameBoard = document.querySelector('#gameboard');
+        this.loseScreen = document.querySelector('#lose-screen');
+        this.winScreen = document.querySelector('#win-screen');
 
         this.gameState = this.initializeGameState();
         console.log(this.gameState);
@@ -22,7 +31,7 @@ class Minesweeper
             let newColumn = [];
             for (let row = 0; row < this.ySize; row++)
             {
-                let newSpace = {bomb: false, revealed: false};
+                let newSpace = {bomb: false, revealed: false, tile: null};
                 newColumn[row] = newSpace;
             }
             tempBoard[column] = newColumn;
@@ -56,43 +65,57 @@ class Minesweeper
             for (let col = 0; col < this.ySize; col++)
             {
                 let newCol = document.createElement("div");
-                newCol.setAttribute("class", "tile");
-                if (this.gameState[col][row].bomb == true)
-                {
-                    newCol.innerHTML = "B";
-                }
-                else
-                {
-                    newCol.innerHTML = this.numberAdjacentBombs(col, row);
-                }
-                //let newTile = document.createElement("span");
-                //newTile.setAttribute('class', 'tile');
-                //newTile.setAttribute("data-x", col);
-                //newTile.setAttribute("data-y", row);
-                //newCol.appendChild(newTile);
+                newCol.setAttribute("class", "tile hidden");
+                newCol.setAttribute("data-x", col);
+                newCol.setAttribute("data-y", row);
+                
+                // I put this in just so the DIV size will be correct later
+                let newSpan = document.createElement("span");
+                newSpan.innerHTML = "?";
+                newSpan.style.visibility = "hidden";
+                
+                newCol.appendChild(newSpan);
+                this.gameState[col][row].tile = newCol;
                 newRow.appendChild(newCol);
             }
             this.gameBoard.appendChild(newRow);
         }
+
+        this.setUpTileListeners();
     }
+
+    isBomb(tileX, tileY)
+    {
+        return(this.gameState[tileX][tileY].bomb);
+    }
+
+
+    isRevealed(tileX, tileY)
+    {
+        return(this.gameState[tileX][tileY].bomb);
+    }
+
+
+
     
     // this is used to determine how many bombs are adjacent to a space
     numberAdjacentBombs(xSpace, ySpace)
     {
         let count = 0;
         
-        console.log(xSpace, ySpace);
+        
         // I could reduce the number of lines by injecting this directly into the for loops
-        let colStart = Math.max(ySpace - 1, 0);
-        let colEnd = Math.min(ySpace + 1, (this.ySize-1));
-        let rowStart = Math.max(xSpace - 1, 0);
-        let rowEnd = Math.min(xSpace + 1, (this.xSize-1));
+        let colStart = Math.max((ySpace - 1), 0);
+        let colEnd = Math.min((ySpace + 1), (this.ySize-1));
+        let rowStart = Math.max((xSpace - 1), 0);
+        let rowEnd = Math.min((xSpace + 1), (this.xSize-1));
+
 
         for (let col = colStart; col <= colEnd; col++)
         {
             for (let row = rowStart; row <= rowEnd; row++)
             {
-                if (this.gameState[row][col].bomb == true)
+                if (this.isBomb(row, col))
                 {
                     count++;
                 }
@@ -101,14 +124,92 @@ class Minesweeper
         return count;
     }
 
+    getTargetTile(x, y)
+    {
+        return (this.gameState[x][y].tile);
+    }
+
+    revealSpace(tileX, tileY)
+    {
+        let targetTile = this.getTargetTile(tileX, tileY);
+
+        this.gameState[tileX][tileY].revealed = true;
+        this.revealedTiles++;
+
+        targetTile.setAttribute("class", "tile reveal");
+        
+        let adjBombs = this.numberAdjacentBombs(tileX, tileY);
+        
+        // TODO: reveal more tiles if adjacent bombs is 0
+        
+        if (adjBombs != 0)
+        {
+            targetTile.innerHTML = "";
+            let newSpan = document.createElement("span");
+            newSpan.innerHTML = adjBombs;
+            
+            targetTile.appendChild(newSpan);
+        }
+
+        //remove listener (for now)
+        event.target.removeEventListener("click", handleTurn);
+    }
+
+    processMove(tileX,tileY)
+    {
+        let targetTile = this.getTargetTile(tileX,tileY);
+
+        console.log("Clicked " + tileX + ", " + tileY);
+            
+        if (this.isBomb(tileX, tileY))
+        {
+            targetTile.setAttribute("class", "tile bomb");
+            targetTile.innerHTML = "";
+            let newSpan = document.createElement("span");
+            newSpan.innerHTML = "B";
+            targetTile.appendChild(newSpan);
+            this.loseScreen.setAttribute("class", "show");
+        }
+        else if (!this.isBomb(tileX, tileY) && !this.isRevealed(tileX, tileY))
+        {
+            this.revealSpace(tileX, tileY);
+        }
+    }
+
+    setUpTileListeners()
+    {
+        // This method sets up event listeners for tiles. It is called when we
+        // start a new game. It must find all the tiles and apply event listeners
+        // to them.
+        let tileElements = document.querySelectorAll(".tile");
+        for (let tile of tileElements)
+        {
+            tile.addEventListener("click", handleTurn);
+        }
+    }
+
+    winCheck()
+    {
+        if ((this.bombCount + this.revealedTiles) == (this.xSize * this.ySize))
+        {
+            this.winScreen.setAttribute("class", "show");
+        }
+    }
 }
 
 document.addEventListener("DOMContentLoaded", function(event)
     {            
-        currentGame = new Minesweeper(10,10,10);
+        currentGame = new Minesweeper(5,5,5);
         currentGame.setupGameBoard();
         
     }
     );
 
-let currentGame;
+function handleTurn(event)
+{
+    // get the X, Y from the space; I added the parseint because I had problems w/ stirngs
+    let tileX = parseInt(event.target.getAttribute("data-x"));
+    let tileY = parseInt(event.target.getAttribute("data-y"));
+    currentGame.processMove(tileX, tileY);
+    currentGame.winCheck();
+}
